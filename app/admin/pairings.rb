1e29eace04c12bc41ccc5612ee320e5cdce4a91f
Raghu Betina <raghu@firstdraft.com>
2018-05-28 18:55:00 +0000

    Whitelist columns in Pairing for ActiveAdmin

--- app/admin/pairings.rb
+++ app/admin/pairings.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Pairing do
+
+ permit_params :cigar_id, :bottle_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
